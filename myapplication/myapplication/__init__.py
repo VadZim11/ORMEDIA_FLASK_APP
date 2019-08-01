a510889468e44ce8fe34878369562fe334e76607
VadZim <vadzim11@tut.by>
2019-08-01 13:51:30 +0200

    add pagies

--- myapplication/myapplication/__init__.py
+++ myapplication/myapplication/__init__.py
@@ -3,8 +3,20 @@ from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "Hello World"
+def home():
+    return "<h1>Hello World<h1>"
+
+@app.route("/movies ")
+def movies ():
+    return "<h2>This mjvies page<h2>"
+
+@app.route("/courses ")
+def courses ():
+    return "<h2>This courses page<h2>"
+
+@app.route("/weather")
+def weather():
+    return "<h2>This weather page<h2>"
 
 if __name__ == "__main__":  
     app.run()
