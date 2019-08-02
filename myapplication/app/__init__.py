2bbf580aa0ba11fa9aca8955e2a77e210e88ac05
VadZim <vadzim11@tut.by>
2019-08-02 14:07:21 +0200

    add base html

--- myapplication/app/__init__.py
+++ myapplication/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -17,6 +17,3 @@ def courses ():
 @app.route("/weather")
 def weather():
     return render_template("weather.html")
-
-if __name__ == "__main__":
-    app.run()
