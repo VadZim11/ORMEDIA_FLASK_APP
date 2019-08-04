01856fcbf367ba759b7f616202f1dcb3cdea1c0a
VadZim <vadzim11@tut.by>
2019-08-05 00:01:07 +0200

    add select cours

--- myapplication/myapp/courses/courses.py
+++ myapplication/myapp/courses/courses.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request
-from .parsing_nbrb_by import parsing_cours
+from .parsing_nbrb_by import parsing_cours, parsing_cours_all
 
 courses = Blueprint("courses", __name__, template_folder="templates")
 
@@ -9,5 +9,7 @@ def index():
     cur_abbreviation = "USD"
     if request.method == "POST":
         cur_abbreviation = request.form.get("cur_abbreviation")
+        if cur_abbreviation == "ALL":
+            return render_template("courses/courses.html", text=parsing_cours_all())
         return render_template("courses/courses.html", text=parsing_cours(cur_abbreviation))
     return render_template("courses/courses.html")
--- myapplication/myapp/courses/parsing_nbrb_by.py
+++ myapplication/myapp/courses/parsing_nbrb_by.py
@@ -5,3 +5,8 @@ def parsing_cours(Cur_Abbreviation):
     url = f"http://www.nbrb.by/API/ExRates/Rates/{Cur_Abbreviation}?ParamMode=2"
     courses_nbrb = requests.get(url).json()
     return courses_nbrb
+
+def parsing_cours_all():
+    url = "http://www.nbrb.by/API/ExRates/Rates?Periodicity=0"
+    courses_nbrb = requests.get(url).json()
+    return courses_nbrb
