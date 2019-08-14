dbf86dfa52368c42eb7f566f32bc3d8babc89860
VadZim <vadzim11@tut.by>
2019-08-14 11:43:55 +0200

    add wether pre

--- myapplication/myapp/muvies/parsing_afisha_tut_by.py
+++ myapplication/myapp/muvies/parsing_afisha_tut_by.py
@@ -5,40 +5,45 @@ import os
 
 def parsing_afisha():
     url = "https://afisha.tut.by/film"
-    
-    r = requests.get(url)
+    try:
+        r = requests.get(url)
 
-    afisha = BeautifulSoup(r.text, "lxml")
-    h1 = afisha.find("div", {"class": "events-block js-cut_wrapper"})
-    h2 = h1.findAll("ul", {"class": "b-lists list_afisha col-5"})
-    h3 = h1.find("div", {"class": "b-premiers-schedule"})
-    list_delete = h3.findAll("li", {"class": "lists__li"})
+        afisha = BeautifulSoup(r.text, "lxml")
+        h1 = afisha.find("div", {"class": "events-block js-cut_wrapper"})
+        h2 = h1.findAll("ul", {"class": "b-lists list_afisha col-5"})
+        h3 = h1.find("div", {"class": "b-premiers-schedule"})
+        list_delete = h3.findAll("li", {"class": "lists__li"})
 
-    list_name = []
-    for i in h2:
-        list_name += i.findAll("li", {"class": "lists__li"})         
+        list_name = []
+        for i in h2:
+            list_name += i.findAll("li", {"class": "lists__li"})         
 
-    # delete premiers muvies
-    list_delete_muvi = []
-    for i in list_delete:
-        name = i.find("a", {"class": "name"}).text
-        muvi_href = i.find("a", {"class": "media"}).get("href")
-        muvi_image = i.find("img").get("src")
-        d = dict(name = name , muvi_href = muvi_href, muvi_image = muvi_image)
-        list_delete_muvi.append(d)
+        # delete premiers muvies
+        list_delete_muvi = []
+        for i in list_delete:
+            name = i.find("a", {"class": "name"}).text
+            muvi_href = i.find("a", {"class": "media"}).get("href")
+            muvi_image = i.find("img").get("src")
+            d = dict(name = name , muvi_href = muvi_href, muvi_image = muvi_image)
+            list_delete_muvi.append(d)
 
-    list_muvi = []
-    for i in list_name:
-        name = i.find("a", {"class": "name"}).text
-        muvi_href = i.find("a", {"class": "media"}).get("href")
-        muvi_image = i.find("img").get("src")
-        d = dict(name = name , muvi_href = muvi_href, muvi_image = muvi_image)
-        list_muvi.append(d)  
+        list_muvi = []
+        for i in list_name:
+            name = i.find("a", {"class": "name"}).text
+            muvi_href = i.find("a", {"class": "media"}).get("href")
+            muvi_image = i.find("img").get("src")
+            d = dict(name = name , muvi_href = muvi_href, muvi_image = muvi_image)
+            list_muvi.append(d)  
         
-    # delete premiers muvies
-    for i in list_delete_muvi:
-        for i2 in list_muvi:
-            if i["muvi_href"] == i2["muvi_href"]:
-                list_muvi.remove(i2)
+        # delete premiers muvies
+        for i in list_delete_muvi:
+            for i2 in list_muvi:
+                if i["muvi_href"] == i2["muvi_href"]:
+                    list_muvi.remove(i2)
+
+    except:
+        list_muvi = []
+        d = dict(name = "Sorry, this page is not available." , muvi_href = "", muvi_image = "")
+        list_muvi.append(d)  
 
     return list_muvi
--- myapplication/myapp/weather/parsing_openweathermap.py
+++ myapplication/myapp/weather/parsing_openweathermap.py
@@ -5,4 +5,7 @@ from .ipa_wether import IPA
 def parsing_wether(city_name):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={city_name},BY&appid={IPA}"
     wether = requests.get(url).json()
-    return wether
+    temp = ""
+    for i in range(len(wether["list"])):
+        temp += str(wether["list"][i]["dt_txt"]) + " - " + str(int(float(wether["list"][i]["main"]["temp"]) - 273.15)) + "\n    "    
+    return temp
