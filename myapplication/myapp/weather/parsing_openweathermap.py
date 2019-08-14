461ea4424823512b288b15f02f4ae86ecd345ac5
VadZim <vadzim11@tut.by>
2019-08-14 17:59:37 +0200

    add wether table

--- myapplication/myapp/weather/parsing_openweathermap.py
+++ myapplication/myapp/weather/parsing_openweathermap.py
@@ -5,10 +5,14 @@ from .ipa_wether import IPA
 def parsing_wether(city_name):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={city_name},BY&appid={IPA}"
     wether = requests.get(url).json()
-    temp = []
-    for i in range(len(wether["list"])):
+    data = []
+    for i in range(6):
         dt_txt = str(wether["list"][i]["dt_txt"])
-        wether_temp = str(int(float(wether["list"][i]["main"]["temp"]) - 273.15))
-        d = dict(dt_txt =dt_txt , wether_temp = wether_temp) 
-        temp.append(d)   
-    return temp
+        temp = str(int(float(wether["list"][i]["main"]["temp"]) - 273.15))
+        wind_speed = str(wether["list"][i]["wind"]["speed"])
+        pressure = str(wether["list"][i]["main"]["pressure"])
+        icon = wether["list"][i]["weather"][0]["icon"]
+        weather_icon = f"http://openweathermap.org/img/wn/{icon}@2x.png"
+        d = dict(dt_txt=dt_txt , temp=temp, wind_speed=wind_speed, pressure=pressure, weather_icon=weather_icon) 
+        data.append(d)   
+    return data
--- myapplication/myapp/weather/weather.py
+++ myapplication/myapp/weather/weather.py
@@ -8,4 +8,4 @@ def index():
     if request.method == "POST":
         city_name = request.form.get("city_name")
         return render_template("weather/weather.html", text=parsing_wether(city_name), city_name=city_name)
-    return render_template("weather/weather.html")
+    return render_template("weather/weather.html", text=parsing_wether("Minsk"), city_name="Minsk")
