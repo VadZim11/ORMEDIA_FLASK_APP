588161a6a28613eaf050dc9c90139b7c75a9d441
VadZim <vadzim11@tut.by>
2019-08-14 15:24:58 +0200

    add wether list

--- myapplication/myapp/weather/parsing_openweathermap.py
+++ myapplication/myapp/weather/parsing_openweathermap.py
@@ -5,7 +5,10 @@ from .ipa_wether import IPA
 def parsing_wether(city_name):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={city_name},BY&appid={IPA}"
     wether = requests.get(url).json()
-    temp = ""
+    temp = []
     for i in range(len(wether["list"])):
-        temp += str(wether["list"][i]["dt_txt"]) + " - " + str(int(float(wether["list"][i]["main"]["temp"]) - 273.15)) + "\n    "    
+        dt_txt = str(wether["list"][i]["dt_txt"])
+        wether_temp = str(int(float(wether["list"][i]["main"]["temp"]) - 273.15))
+        d = dict(dt_txt =dt_txt , wether_temp = wether_temp) 
+        temp.append(d)   
     return temp
--- myapplication/myapp/weather/weather.py
+++ myapplication/myapp/weather/weather.py
@@ -7,5 +7,5 @@ weather = Blueprint("weather", __name__, template_folder="templates")
 def index():
     if request.method == "POST":
         city_name = request.form.get("city_name")
-        return render_template("weather/weather.html", text = parsing_wether(city_name))
+        return render_template("weather/weather.html", text=parsing_wether(city_name), city_name=city_name)
     return render_template("weather/weather.html")
