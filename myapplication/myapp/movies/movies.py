b69edff82f26822148e45dc61fc5ddbaa440acd6
VadZim <vadzim11@tut.by>
2019-08-21 23:45:27 +0200

    add movi pagintion

--- myapplication/myapp/movies/movies.py
+++ myapplication/myapp/movies/movies.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from .parsing_afisha_tut_by import parsing_afisha
 from models import Movies
 from app import db
@@ -8,13 +8,25 @@ movies = Blueprint("movies", __name__, template_folder="templates")
 @movies.route("/")
 def index():
 
+    page = request.args.get("page")
+
+    if page and page.isdigit():
+        page = int(page)
+    else:
+        page = 1
+
     db.create_all()
-    
-    for i in parsing_afisha():
-        movi = Movies(name = i["name"], muvi_href = i["muvi_href"], muvi_image = i["muvi_image"], film_genre = i["film_genre"])
-        db.session.add(movi)
-        db.session.commit()
 
-    movies = Movies.query.all()
+    if Movies.query.all()==[]:            
+        for i in parsing_afisha():
+            movi = Movies(name = i["name"], muvi_href = i["muvi_href"], muvi_image = i["muvi_image"], film_genre = i["film_genre"])
+            db.session.add(movi)
+            db.session.commit()
 
-    return render_template("movies/movies.html", movies = movies)
+        movies = Movies.query
+    else:
+        movies = Movies.query
+
+    pages = movies.paginate(page=page, per_page=4)
+
+    return render_template("movies/movies.html", movies = movies, pages=pages)
