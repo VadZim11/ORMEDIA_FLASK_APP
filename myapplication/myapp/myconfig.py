37abc2ae2df56a98c6dbffedde905105fae5005a
VadZim <vadzim11@tut.by>
2019-08-22 11:47:05 +0200

    add database configuration

--- myapplication/myapp/myconfig.py
+++ myapplication/myapp/myconfig.py
@@ -1,6 +1,6 @@
-from password_sql import PASSWORD
+from password_sql import PASSWORD, HOST, DATABASE, USER
 
 class Myconfiguration(object):
     DEBUG = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://root:{PASSWORD}@localhost/my_flask_app"
+    SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{USER}:{PASSWORD}@{HOST}/{DATABASE}"
