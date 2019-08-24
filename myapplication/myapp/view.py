c9bd22c4e1cfc83871c16cdf514f5f01a00b156f
VadZim <vadzim11@tut.by>
2019-08-24 21:37:54 +0200

    add error page and kastom CSS

--- myapplication/myapp/view.py
+++ myapplication/myapp/view.py
@@ -8,3 +8,7 @@ def home():
 @app.route("/contakts")
 def contakts():
     return render_template("contakts.html")
+
+@app.errorhandler(404)
+def page_error(e):
+    return render_template("404.html"), 404
