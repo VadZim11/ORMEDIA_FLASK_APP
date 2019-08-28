3882c2f634241ee29127e05520855b5ebedd8ca1
VadZim <vadzim11@tut.by>
2019-08-28 16:18:15 +0200

    add movies archive

--- myapplication/myapp/movies/movies.py
+++ myapplication/myapp/movies/movies.py
@@ -6,7 +6,7 @@ import datetime
 
 movies = Blueprint("movies", __name__, template_folder="templates")
 
-@movies.route("/")
+@movies.route("/", methods=["POST", "GET"])
 def index():
     db.create_all()
 
@@ -15,23 +15,49 @@ def index():
             movi = Movies(name = i["name"], muvi_href = i["muvi_href"], muvi_image = i["muvi_image"], film_genre = i["film_genre"])
             db.session.add(movi)
             db.session.commit()
-        movies = Movies.query.filter(Movies.date == datetime.date.today())
+        movies = Movies.query
     else:
         if db.session.query(Movies).filter(Movies.date == datetime.date.today()).first():
-            movies = Movies.query.filter(Movies.date==datetime.date.today())
+            movies = Movies.query
         else:
             for i in parsing_afisha():
                 movi = Movies(name = i["name"], muvi_href = i["muvi_href"], muvi_image = i["muvi_image"], film_genre = i["film_genre"])
                 db.session.add(movi)
                 db.session.commit()
-            movies = Movies.query.filter(Movies.date == datetime.date.today())
+            movies = Movies.query    
+    
+    if request.method == "POST":        
+        date_muvie = request.form.get("date_movie")
 
-    page = request.args.get("page")
-    if page and page.isdigit():
-        page = int(page)
+        page = request.args.get("page")
+        if page and page.isdigit():
+            page = int(page)
+        else:
+            page = 1 
+        
+        pages = movies.filter(Movies.date == date_muvie).paginate(page=page, per_page=4)
+
+        dates = db.session.query(Movies.date).distinct().all()
+
+        return render_template("movies/movies.html", pages=pages, dates=dates, date_muvie=date_muvie)
+    
     else:
-        page = 1 
+        date = request.args.get("date")
+        dates = db.session.query(Movies.date).distinct().all()
+        dates_prev = []
+        for i in dates:
+            dates_prev.append(str(i.date))
+        if date and date in dates_prev:
+            date_muvie = date
+        else:
+            date_muvie =  datetime.date.today()
 
-    pages = movies.paginate(page=page, per_page=4)
+        page = request.args.get("page")
+        if page and page.isdigit():
+            page = int(page)
+        else:
+            page = 1 
 
-    return render_template("movies/movies.html", pages=pages)
+        pages = movies.filter(Movies.date == date_muvie).paginate(page=page, per_page=4)
+
+        return render_template("movies/movies.html", pages=pages, dates=dates, date_muvie=date_muvie)
